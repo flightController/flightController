67e480b45d8ff2e57364468f5e0df42236c78356
Patrick <patrick@elsasser.me>
2016-12-03 15:09:28 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,7 @@
 <?php
     require_once '../app/init.php';
+
+include_once 'lib/FlightAwareJsonAdapter.php';
+
+$adapter = new FlightAwareJsonAdapter('jenzer', 'APIKEY');
+$adapter->updateAirportDatabase();
