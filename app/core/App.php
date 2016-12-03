322516baead9d99f47d6e291712671521c1d8aaf
Mauro <maurostehle@gmail.com>
2016-12-03 16:29:24 +0100

    mvc model

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,41 @@
 
 class App{
 
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+
+    /**
+     * App constructor.
+     */
     public function __construct()
     {
-        echo 'App';
+        $url = $this->parseURL();
+
+        if(file_exists('../app/controller/' . $url[0] . '.php')){
+            $this -> controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controller/' . $this ->controller . '.php';
+        $this -> controller = new $this -> controller;
+        if(isset($url[1])){
+            if(method_exists($this -> controller, $url[1])){
+                $this -> method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        $this -> params = $url ? array_values($url) : [0];
+
+        call_user_func_array([$this ->controller, $this-> method], $this -> params);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])) {
+            return $url = explode('/',filter_var(rtrim($_GET['url'], '/'),FILTER_SANITIZE_URL));
+        }
+
     }
 }
