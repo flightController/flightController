c79aa1a5bc43710eb594ba1cfffef3fd3a52768e
Humbel <humbi1992@gmail.com>
2016-12-07 15:33:01 +0100

    Script Bootstrap included

--- app/view/flight/FlightListView.php
+++ app/view/flight/FlightListView.php
@@ -1,3 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Sky Tracker</title>
+    <meta name="description" content="Flugtracker ABB Technikerschule">
+</head>
 
-echo 'FlightListView';
+<body>
+
+<div class="container flightcontainer">
+    <div class="row">
+        <div class="col-lg-12 flightlist">
+            <h1>Berlin</h1>
+            <div class="row">
+                <div class="col-lg-9">
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                </div>
+                <div class="col-lg-3">
+                    <img src="../images/berlinthumbnail.jpg"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 descriptionbutton">
+
+                </div>
+                <div class="col-lg-3">
+                </div>
+                <div class="col-lg-3">
+                </div>
+                <div class="col-lg-3">
+                </div>
+        </div>
+        <div class="col-lg-12 flightlist">
+            <h1>New York</h1>
+        </div>
+        <div class="col-lg-12 flightlist">
+            <h1>London</h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
