bd00b44d34e616dff49b6a7a7f95d03c73e7c153
Humbel <humbi1992@gmail.com>
2016-12-09 17:39:14 +0100

    Script Bootstrap included

--- public/index.php
+++ public/index.php
@@ -9,17 +9,21 @@ include_once '../resources/keys.php';
     <title>SkyTracker</title>
     <meta name="description" content="SkyTracker Semesterarbeit 5. Semester der ABBTS in Baden">
     <meta name="author" content="Andreas Reimann, Mauro Stehle, Patrick Elsasser und Sven Humbel">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <!-- Own Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 </head>
 <body>
 <?php
+$host = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+if($host != 'skytracker.local/login') {
 include_once '../app/view/menu/menu.php';
+}
+
 ?>
 <div class="container body-content">
 
