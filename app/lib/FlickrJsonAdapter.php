1b2b13fa660a05c5b9726d4a93bf926862656fef
Patrick <patrick@elsasser.me>
2016-12-10 14:37:29 +0100

    FlickrJsonAdapter created

--- app/lib/FlickrJsonAdapter.php
+++ app/lib/FlickrJsonAdapter.php
@@ -1,28 +1,56 @@
 <?php
 
-class FlickrAdapter
+class FlickrJsonAdapter
 {
-    private $username;
     private $apiKey;
     private static $baseUrl = 'https://www.flickr.com/services/rest/?';
-    private $method = 'flickr.photos.search';
-    private $format = 'json';
-    private $tags;
-    private $content_type = '1';
-    private $pages = '1';
+    private $method = '&method=flickr.photos.search';
+    private $format = '&format=json';
+    private $tags = '&tags=';
+    private $content_type = '&content_type=1';
+    private $pages = '&content_type=1';
+    private $per_page = '&per_page=';
 
     /**
-     * FlickrAdapter constructor.
-     * @param $username
+     * FlickrJsonAdapter constructor.
      * @param $apiKey
      */
-    public function __construct($username, $apiKey)
+    public function __construct($apiKey)
     {
-        $this->username = $username;
         $this->apiKey = $apiKey;
     }
+    
+    
+   private function getPictures(string $city, string $howmany, string $size, string $format)
+   {
+       $photo_url_array = array();
+       $url = (self::$baseUrl . $this->method . $this->tags . $city . $this->format . $this->content_type . $this->per_page . $howmany . '&api_key=' . $this->apiKey . '&nojsoncallback=1');
+       $response = json_decode(file_get_contents($url));
+       $photo_array = $response->photos->photo;
 
-    public function getPicture($city, $number) {
+       foreach ($photo_array as $single_photo) {
 
-    }
+           $farm_id = $single_photo->farm;
+           $server_id = $single_photo->server;
+           $photo_id = $single_photo->id;
+           $secret_id = $single_photo->secret;
+
+
+           $title = $single_photo->title;
+
+           $photo_url = 'http://farm' . $farm_id . '.staticflickr.com/' . $server_id . '/' . $photo_id . '_' . $secret_id . $size . '.' . $format;;
+
+
+           $photo_url_array[] = array($photo_url);
+       }
+        return $photo_url_array;
+   }
+    public function getSmallPictures(string $city, string $howmany) {
+        return $this->getPictures($city, $howmany, '_m', 'jpg');
+       }
+
+       public function getFullPictures(string $city, string $howmany) {
+        return $this->getPictures($city, $howmany, '', 'jpg');
+       }
+
 }
--- public/test.php
+++ public/test.php
@@ -1,7 +1,11 @@
 <?php
 
 
-include_once '../lib/FlightAwareJsonAdapter.php';
+include_once '../app/lib/FlightAwareJsonAdapter.php';
+include_once '../app/lib/FlickrJsonAdapter.php';
+include_once '../resources/keys.php';
 
-$adapter = new FlightAwareJsonAdapter('jenzer', 'APIKEY');
-$adapter->getAllAirportName();
+/*$adapter = new FlightAwareJsonAdapter('jenzer', 'APIKEY');
+$adapter->getAllAirportName();*/
+$flickr = new FlickrJsonAdapter(FLICKR_API_KEY);
+var_dump($flickr->getFullPictures('HongKong','5'));
